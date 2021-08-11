34261defe957e32c10151f09d16ce278ca4d379d
Carlos <se_carlosprieto@hotmail.com>
2021-08-11 22:57:28 +0200

    character model

---
+++ la_resistencia/Assets/Models/Character.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character
+{
+    public enum CharacterRole
+    {
+        good,
+        bad
+    }
+
+    public class CharacterName
+    {
+        /*public static bool isGood(CharacterRole role)
+        {
+            if (CharacterRole.good)
+            {
+                return true;
+
+            }
+            else if (CharacterRole.bad)
+            {
+                return false;
+            }
+        }*/
+    }
+}
